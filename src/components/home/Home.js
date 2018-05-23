614a7a7d9af2e997c4651bb3abc59399a39758df
David Wiszowaty <wiszowatyd@gmail.com>
2018-05-22 22:22:05 -0500

    Current month and year is now displayed on calendar
    - adds day names to calendar

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,15 +1,35 @@
 import React, {Component} from 'react';
 
 export default class App extends Component {
+    renderMonth = () => {
+        const monthNames = ["January", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November", "December"
+        ];
+        let date = new Date();
+
+        return (<div>
+            {monthNames[date.getMonth()] + " " + date.getFullYear()}
+        </div>)
+    };
+
     render() {
         return (
             <table>
                 <thead>
                     <tr>
-                        <td colspan="7">Month</td>
+                        <td colSpan="7">{this.renderMonth()}</td>
                     </tr>
                 </thead>
                 <tbody>
+                    <tr>
+                        <td>SUN</td>
+                        <td>MON</td>
+                        <td>TUE</td>
+                        <td>WED</td>
+                        <td>THU</td>
+                        <td>FRI</td>
+                        <td>SAT</td>
+                    </tr>
                     <tr>
                         <td>day</td>
                         <td>day</td>
--- src/components/home/__tests__/Home.test.js
+++ src/components/home/__tests__/Home.test.js
@@ -4,6 +4,11 @@ import Home from "../Home";
 
 describe('Home', () => {
     let wrapper;
+    const mockedDate = new Date(2018, 3, 20);
+    const originalDate = Date;
+
+    global.Date = jest.fn(() => mockedDate);
+    global.Date.setDate = originalDate.setDate;
 
     beforeEach(() => {
         wrapper = shallow(<Home />);
@@ -26,19 +31,36 @@ describe('Home', () => {
             expect(thead.length).toBe(1);
             expect(thead.find('tr').length).toBe(1);
             expect(thead.find('td').length).toBe(1);
-            expect(thead.find('td').prop("colspan")).toBe("7");
-            expect(thead.find('td').text()).toBe("Month");
+            expect(thead.find('td').prop("colSpan")).toBe("7");
+        });
+
+        it('correct month and year', () => {
+            let thead = table.find('thead');
+
+            expect(thead.find('td').text()).toBe("April 2018");
         });
 
         it('day rows', () => {
             let row = table.find('tbody').find('tr');
 
-            expect(row.length).toBe(5);
+            expect(row.length).toBe(6);
             expect(row.at(0).find('td').length).toBe(7);
             expect(row.at(1).find('td').length).toBe(7);
             expect(row.at(2).find('td').length).toBe(7);
             expect(row.at(3).find('td').length).toBe(7);
             expect(row.at(4).find('td').length).toBe(7);
+        });
+
+        it('day label row', () => {
+            let row = table.find('tbody').find('tr').at(0);
+
+            expect(row.find('td').at(0).text()).toBe("SUN");
+            expect(row.find('td').at(1).text()).toBe("MON");
+            expect(row.find('td').at(2).text()).toBe("TUE");
+            expect(row.find('td').at(3).text()).toBe("WED");
+            expect(row.find('td').at(4).text()).toBe("THU");
+            expect(row.find('td').at(5).text()).toBe("FRI");
+            expect(row.find('td').at(6).text()).toBe("SAT");
         });
     });
 });
